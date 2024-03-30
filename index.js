2fbb5fbe44f2c4e04a8db4ac962c917dbb59edca
Benhin <benhinmwendwa7@gmail.com>
2024-03-30 17:51:35 +0300

    loops

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+function  writeCards(name,event){
+    let value = [];
+    for(let i = 0;i<name.length;i++)
+    {
+    
+        value.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    return value
+}
+console.log ("Guadalupe","Ollie","Aki")
+function countDown(count) {
+    while (count >=0) { 
+        console.log(count);
+        count-=1
+    }
+}
+
+count(10);
